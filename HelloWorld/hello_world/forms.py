830ffba7b86441a67534649fae1f7f1c382b8169
P.Seferyniak <p.seferyniak@gmail.com>
2023-03-11 18:42:49 +0100

    Update forms.py

---
+++ HelloWorld/hello_world/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .models import Category, Publisher
+
+class PublisherForm(forms.ModelForm):
+    class Meta:
+        model: Publisher
+        exclude = ['id']
+        labels = {
+            'name': 'Name',
+            'country': 'Country'
+        }
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model: Category
+        exclude = ['id']
+        labels = {
+            'type': 'Category type'
+        }
