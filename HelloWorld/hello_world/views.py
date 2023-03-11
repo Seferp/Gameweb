f0754842227806bb43b825808e4e021e1fcd317c
P.Seferyniak <p.seferyniak@gmail.com>
2023-03-11 18:42:39 +0100

    Update forms.py

--- HelloWorld/hello_world/views.py
+++ HelloWorld/hello_world/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
 
+from . import models
+from . import forms
+from .models import Game, GameCategory, Category, Publisher
+from .forms import PublisherForm, CategoryForm
 # Create your views here.
 
 def hello(request):
     return render(request, 'hello_world/hello.html')
+
+class PublisherView(CreateView):
+    model = models.Publisher
+    form_class = forms.PublisherForm
+    success_url = ''  #Do uzupełnienia
+
+    def get(self, **kwargs):
