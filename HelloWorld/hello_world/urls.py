14b3812cc8bf3e0945f6c715419da1e40738f19e
P.Seferyniak <p.seferyniak@gmail.com>
2023-03-11 23:55:41 +0100

    Crete CRUD

--- HelloWorld/hello_world/urls.py
+++ HelloWorld/hello_world/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.hello),
     path("create-publisher", views.CreatePublisherView.as_view(), name="create-publisher"),
+    path("<int:pk>/", views.PublisherDetail.as_view(), name="publisher-detail"),
+    path("<int:pk>/update", views.PublisherUpdate.as_view(), name="publisher-update"),
+    path("<int:pk>/delete", views.PublisherDelete.as_view(), name="publisher-delete"),
 ]
--- HelloWorld/hello_world/views.py
+++ HelloWorld/hello_world/views.py
@@ -2,7 +2,7 @@ from logging import getLogger
 
 from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
 from . import models
 from . import forms
@@ -21,6 +21,21 @@ class CreatePublisherView(CreateView):
     template_name = "hello_world/create_publisher.html"
     success_url = reverse_lazy(hello)
 
+class PublisherDetail(DetailView):
+    model = Publisher
+    form_class = forms.CreatePublisherForm
+    template_name = "hello_world/publisher_detail.html"
+
+class PublisherUpdate(UpdateView):
+    model = Publisher
+    template_name = "hello_world/publisher_update.html"
+    fields = ['name', 'country']
+    success_url = reverse_lazy(hello)
+
+class PublisherDelete(DeleteView):
+    model = Publisher
+    template_name = "hello_world/publisher_delete.html"
+    success_url = reverse_lazy(hello)
 
 
 class CategoryView(CreateView):
