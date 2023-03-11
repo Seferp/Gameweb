f8912b9729c563c1b7fed5a3fb36d1011b5b9147
P.Seferyniak <p.seferyniak@gmail.com>
2023-03-11 19:26:19 +0100

    Update forms.py

--- HelloWorld/hello_world/views.py
+++ HelloWorld/hello_world/views.py
@@ -1,3 +1,6 @@
+from logging import getLogger
+
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DeleteView
 
@@ -7,12 +10,19 @@ from .models import Game, GameCategory, Category, Publisher
 from .forms import PublisherForm, CategoryForm
 # Create your views here.
 
+LOGGER = getLogger()
+
 def hello(request):
     return render(request, 'hello_world/hello.html')
 
 class PublisherView(CreateView):
     model = models.Publisher
     form_class = forms.PublisherForm
-    success_url = ''  #Do uzupełnienia
+    success_url = reverse_lazy('')  #Do uzupełnienia
 
-    def get(self, **kwargs):
+
+class CategoryView(CreateView):
+    model = models.Category
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('')  #Do uzupełnienia
+
